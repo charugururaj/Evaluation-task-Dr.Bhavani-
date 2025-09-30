f4b48e759f4590c946871a340591bdc22f3816f3
charugururaj <charugururaj@gmail.com>
2025-09-29 22:24:02 -0400

    Update CG_CLIF_Vitals_EvaluationTask_V01.R

--- CG_CLIF_Vitals_EvaluationTask_V01.R
+++ CG_CLIF_Vitals_EvaluationTask_V01.R
@@ -9,9 +9,9 @@ pacman::p_load(readr, dplyr, stringr, lubridate, janitor)
 
 # Setting paths to the datafiles
 
-path_chartevents <- "C:\\Users\\charu\\OneDrive - Emory University\\Desktop\\MSPH\\Resume\\Job Applications\\Aug-2025\\Emory SoM\\Dr. Bhavani\\Evaluation Task\\chartevents3.csv"
-path_mappings <- "C:\\Users\\charu\\OneDrive - Emory University\\Desktop\\MSPH\\Resume\\Job Applications\\Aug-2025\\Emory SoM\\Dr. Bhavani\\Evaluation Task\\mappings.csv"
-path_out <- "C:\\Users\\charu\\OneDrive - Emory University\\Desktop\\MSPH\\Resume\\Job Applications\\Aug-2025\\Emory SoM\\Dr. Bhavani\\Evaluation Task\\clif_vitals.csv"
+path_chartevents <- "C:\\Users\\... \\Evaluation Task\\chartevents3.csv"
+path_mappings <- "C:\\Users\\... \\Evaluation Task\\mappings.csv"
+path_out <- "C:\\Users\\...\\Evaluation Task\\clif_vitals.csv"
 
 # Loading data
 
@@ -56,41 +56,53 @@ for (src in names(rename_candidates)) {
     names(df)[names(df) == src] <- dst
   }
 }
+# removing outliers
+
+# Defining limits
+
+limits <- tibble::tribble(
+  ~vital_category,      ~lower_limit, ~upper_limit,
+  "height_inches",               30,           96,
+  "weight_kg",                   30,         1100,
+  "sbp",                          0,          300,
+  "dbp",                          0,          200,
+  "map",                          0,          250,
+  "pulse",                        0,          300,
+  "respiratory_rate",             0,           60,
+  "spo2",                        50,          100,
+  "temp_c",                      32,           44
+)
+
+# applying outlier rules
+
+df_vitals <- df %>% 
+  left_join(limits, by= "vital_category") %>% 
+  mutate(
+    outlier_flag = !is.na(vital_value) & !is.na(lower_limit) & !is.na(upper_limit) &
+      (vital_value < lower_limit | vital_value > upper_limit),
+    vital_value  = ifelse(outlier_flag, NA_real_, vital_value)
+  )
+
+# removing duplicates
+# Keeping one value per hospitalization, timestamp, vital, measurement site.
+
+n_before <- nrow(df_vitals)
+df_vitals2 <- df_vitals %>% distinct(hospitalization_id, recorded_dttm, vital_name, vital_category,
+                                     vital_value, meas_site_name)
+n_exact_removed <- n_before - nrow(df_vitals2)
 
 # implement the required datatypes
 
-df <- df %>%
+clif_vitals <- df_vitals2 %>%
   mutate(
     hospitalization_id = as.character(hospitalization_id),
-    recorded_dttm      = suppressWarnings(lubridate::ymd_hms(recorded_dttm)), # parse datetime
+    recorded_dttm      = as.POSIXct(recorded_dttm, tz = "UTC"),
     vital_name         = as.character(vital_name),
     vital_category     = as.character(vital_category),
     vital_value        = as.numeric(vital_value),
     meas_site_name     = as.character(meas_site_name)
   )
 
-# keeping only the required columns for the CLIF vitals category
-
-clif_vitals <- df %>%
-  select(
-    hospitalization_id,
-    recorded_dttm,
-    vital_name,
-    vital_category,
-    vital_value,
-    meas_site_name
-  )
-
-glimpse(clif_vitals)
-
-write_csv(clif_vitals, path_out)
-message("Wrote: ", normalizePath(path_out))
+arrow::write_parquet(clif_vitals, path_out)
 
 
-
-# count the number of unique values in ID
-
-num_unique_id <- length(unique(chartevents_3$subject_id))
-print(num_unique_id)
-
-colnames(chartevents_3) <- c("column_one")
